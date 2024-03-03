ac50ddab1d40d254d3c65f840e57d29a5e3501a8
hexoN <aliakbaripour00@gmail.com>
2024-03-03 10:25:47 +0330

    localStorage + Debugs Add localStorage to save the data Fix bug where localStorage = null Fix clear button not working due to conflicting with localStorage

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-let calculation = '';
+let calculation = localStorage.getItem('storedCalculation') || '';
 
 function updateCalculation(value) {
-
   calculation += value;
   console.log(calculation);
+  localStorage.setItem('storedCalculation', calculation);
+}
+
+function clearCalculation() {
+  calculation = '';
+  console.log(calculation);
+  localStorage.setItem('storedCalculation', calculation);
 }
