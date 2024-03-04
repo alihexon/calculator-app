316779805fe51e2742fa5d62a72156b2fe852574
hexoN <aliakbaripour00@gmail.com>
2024-03-04 15:11:41 +0330

    DOM update Create a calculation display to show the calculation result on webpage set display to localStorage if there's any

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 let calculation = localStorage.getItem('storedCalculation') || '';
+const calculationDisplay = document.querySelector('.calculation-display');
+
+if (calculation === localStorage.getItem('storedCalculation')) {
+  calculationDisplay.innerHTML = calculation;
+}
 
 function updateCalculation(value) {
   calculation += value;
-  console.log(calculation);
+  calculationDisplay.innerHTML = calculation;
   localStorage.setItem('storedCalculation', calculation);
 }
 
 function clearCalculation() {
   calculation = '';
-  console.log(calculation);
+  calculationDisplay.innerHTML = calculation;
   localStorage.setItem('storedCalculation', calculation);
+}
+
+function doCalculation() {
+  calculation = eval(calculation);
+  calculationDisplay.innerHTML = calculation;
 }
