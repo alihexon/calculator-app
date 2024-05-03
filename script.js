892a91d066ad49f312604b673f54b87f0439e3d9
hexoN <aliakbaripour00@gmail.com>
2024-05-03 13:35:33 +0330

    Replaced innerHTML with textContent

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 let calculation = localStorage.getItem("storedCalculation") || "0";
 const calculationDisplay = document.querySelector(".screen");
 
-calculationDisplay.innerHTML = calculation;
+calculationDisplay.textContent = calculation;
 
 function updateCalculation(value) {
   calculation += value;
-  calculationDisplay.innerHTML = calculation;
+  calculationDisplay.textContent = calculation;
 }
 
 function clearCalculation() {
   calculation = "0";
-  calculationDisplay.innerHTML = calculation;
+  calculationDisplay.textContent = calculation;
   localStorage.setItem("storedCalculation", calculation);
 }
 
 function doCalculation() {
   calculation = eval(calculation);
-  calculationDisplay.innerHTML = calculation;
+  calculationDisplay.textContent = calculation;
   localStorage.setItem("storedCalculation", calculation);
 }
